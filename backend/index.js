eea0bc6ec368ba096ee1f8e1d91b5d03941b2a38
nafi-ullah <shafinnafiullah@gmail.com>
2024-02-26 03:03:42 +0600

    backend skeleton

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,27 @@
- console.log("hello world")
+const express = require("express");
+
+const mydatabase = require("mongoose");
+
+const app = express();
+
+
+const PORT = 3000;
+const DB =
+  "mongodb+srv://managernafi:dmc54321@cluster0.7dvhcpm.mongodb.net/?retryWrites=true&w=majority";
+
+app.use(express.json());
+
+
+mydatabase
+  .connect(DB)
+  .then(() => {
+    console.log("Daatabase Connected successfully");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running  on ${PORT}`);
+});
+
